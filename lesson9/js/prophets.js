21a48d60320bfa48098209a4912b7f1a7e6a729d
b3q <darrenb3q@byui.edu>
2021-03-02 20:49:03 -0700

    20210302_2049

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,31 @@
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (jsonObject){
+    const prophets = jsonObject['prophets'];
+    console.table(jsonObject); // temp check output
+    for(let i = 0; i < prophets.length; i++) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let h4 = document.createElement('h4');
+        let h5 = document.createElement('h4');
+        let img = document.createElement('img');
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        h4.textContent = 'Birthday: ' + prophets[i].birthdate;
+        h5.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        img.setAttribute('src', prophets[i].imageurl);
+ 
+        card.appendChild(h2);
+        card.appendChild(h4);
+        card.appendChild(h5);
+        card.appendChild(img);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+  })
+
+  
