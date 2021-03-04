60ac287298d6e3bca6a98ad4be4d330faee9759e
b3q <darrenb3q@byui.edu>
2021-03-04 14:34:44 -0700

    20210304_1434

---
+++ lesson9/js/towns-BKP.js
@@ -0,0 +1,44 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+// const requestURL = 'js/towns.json';
+
+fetch(requestURL)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (jsonObject){
+    const towns = jsonObject['towns'];
+    console.table(jsonObject); // temp check output
+    for(let i = 0; i < towns.length; i++) {
+        let card = document.createElement('article');
+        let cardinfo = document.createElement('div');
+        cardinfo.className = "cardinfo";
+        let cardimg = document.createElement('div');
+        cardimg.className = "cardimg";
+        let h2 = document.createElement('h2');
+        let h4 = document.createElement('h4');
+        let year = document.createElement('p');
+        let population = document.createElement('p');
+        let rainfall = document.createElement('p');
+        let img = document.createElement('img');
+
+        h2.textContent = towns[i].name;
+        h4.textContent = towns[i].motto;
+        year.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        population.textContent = 'Population: ' + towns[i].currentPopulation;
+        rainfall.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
+        img.setAttribute('src', 'img/' + towns[i].photo);
+
+        card.appendChild(cardimg);
+        card.appendChild(cardinfo);
+        cardimg.appendChild(img);
+        cardinfo.appendChild(h2);
+        cardinfo.appendChild(h4);
+        cardinfo.appendChild(year);
+        cardinfo.appendChild(population);
+        cardinfo.appendChild(rainfall);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+  })
+
+  
