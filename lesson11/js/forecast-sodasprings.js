f6de93eb4e99c0d57cf6b69bb160bc42a1bcdb6e
b3q <darrenb3q@byui.edu>
2021-03-15 20:59:31 -0600

    20210315_2059

---
+++ lesson11/js/forecast-sodasprings.js
@@ -0,0 +1,35 @@
+const requestURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=790367f1eb9bb74890e1a2233f72b32f&units=imperial";
+
+fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        const days = jsObject['list'];
+        for(let i = 0; i < days.length; i++) {
+            let pattern = "18:00:00";
+            let patfound = days[i].dt_txt;
+            if (patfound.includes(pattern)) {
+                let daycard = document.createElement('div');
+                daycard.className = "daycard";
+                let daytitle = document.createElement('div');
+                daytitle.className = "daytitle";
+                let datetime = days[i].dt_txt;
+                let date = datetime.substring(0, 10);
+                let utc = new Date(date).toUTCString();
+                let dayofweek = utc.substring(0, 3);
+                daytitle.textContent = dayofweek;
+                let img = document.createElement('img');
+                img.setAttribute('src', `https://openweathermap.org/img/wn/${days[i].weather[0].icon}.png`);
+                let daytemp = document.createElement('div');
+                daytemp.textContent = `${days[i].main.temp}°F`;
+                daytemp.className = "daytemp";
+
+                daycard.appendChild(daytitle);
+                daycard.appendChild(img);
+                daycard.appendChild(daytemp);
+
+                document.querySelector('section.fiveday-section').appendChild(daycard);
+            }
+        }
+
+    });
---
+++ lesson11/js/weather-fishhaven.js
@@ -0,0 +1,29 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=790367f1eb9bb74890e1a2233f72b32f&units=imperial"
+
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current-weather').textContent = jsObject.weather[0].main;
+        document.getElementById('high-temp').textContent = jsObject.main.temp_max;
+        document.getElementById('humid').textContent = jsObject.main.humidity;
+        document.getElementById('windspeed').textContent = jsObject.wind.speed;
+        const htemp = parseFloat(jsObject.main.temp_max);
+        const ltemp = parseFloat(jsObject.main.temp_min);
+        const avtemp = (htemp + ltemp) / 2;
+        const wspeed = parseFloat(jsObject.wind.speed);
+        if (avtemp > 50 || wspeed < 3) {
+            let f = "N/A";
+            wchill = f;
+        } else {
+            let f = 35.74 + 0.6215 * avtemp - 35.75 * Math.pow(wspeed, 0.16) + 0.5275 * avtemp * Math.pow(wspeed, 0.16);
+            wchill = f.toFixed(1) + "&#8457;";
+        }
+        document.getElementById('windchill').innerHTML = wchill;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        // document.getElementById('imagesrc').textContent = imagesrc;
+        // document.getElementById('icon').setAttribute('src', imagesrc);
+        // document.getElementById('icon').setAttribute('alt', desc);
+    });
